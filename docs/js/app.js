55e775d2c5c778891d8baae2a037a11c94c56e69
Michal Szwajgier <michalszwajgier@gmail.com>
2021-09-30 17:22:35 +0200

    Change back to dist

--- docs/js/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import { initHomePage } from './home.js';
-import { initLocalWeather } from './localWeather.js';
-import { initCitiesForm } from './citiesForm.js';
-
-initHomePage();
-initLocalWeather();
-initCitiesForm();
